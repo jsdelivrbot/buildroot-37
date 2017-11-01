96b04826d4bc7234922fd689875d302fc16b3aaf
Falaleev Mikhail <falaleev@synopsys.com>
2017-11-01 14:56:10 +0300

    add eembs_multibench package

---
+++ package/eembc_multibench/eembc_multibench.mk
@@ -0,0 +1,48 @@
+# EEMBC_MULTIBENCH_VERSION = master
+# EEMBC_MULTIBENCH_SITE = git@gitsnps.internal.synopsys.com:abrodkin/eembc_multibench.git
+# EEMBC_MULTIBENCH_SITE_METHOD = git
+#
+#
+# TARGET_CONFIGURE_OPTS = \
+# 		'clean' \
+# 		'build' \
+# 		TARGET=linux \
+# 		TOOLCHAIN=arc_linux \
+# 		TOOLS=$(HOST_DIR)/usr
+#
+#
+# define EEMBC_MULTIBENCH_BUILD_CMDS
+#     # $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+# 		@echo Hello $(TARGET_MAKE_ENV) $(MAKE) -C $(@D) DESTDIR="$(TARGET_DIR)" install
+# endef
+#
+# $(eval $(generic-package))
+
+#############################################################
+#
+# MultiBench
+#
+#############################################################
+EEMBC_MULTIBENCH_VERSION = master
+EEMBC_MULTIBENCH_SITE = git@gitsnps.internal.synopsys.com:abrodkin/eembc_multibench.git
+# EEMBC_MULTIBENCH_SITE = https://gitsnps.internal.synopsys.com/abrodkin/eembc_multibench/repository
+# EEMBC_MULTIBENCH_SOURCE = archive.tar.gz
+EEMBC_MULTIBENCH_SITE_METHOD = git
+# EEMBC_MULTIBENCH_DL_OPTS = '--header "Private-Token: "'
+# MULTIBENCH_INSTALL_STAGING = YES
+EEMBC_MULTIBENCH_DEPENDENCIES = expect
+define EEMBC_MULTIBENCH_BUILD_CMDS
+    $(MAKE) -C $(@D) build TARGET=linux TOOLCHAIN=arc_linux TOOLS=$(HOST_DIR)/usr/
+endef
+# define MULTIBENCH_INSTALL_STAGING_CMDS
+#     $(INSTALL) -D -m 0755 $(@D)/libfoo.a $(STAGING_DIR)/usr/lib/libfoo.a
+#     $(INSTALL) -D -m 0644 $(@D)/foo.h $(STAGING_DIR)/usr/include/foo.h
+#     $(INSTALL) -D -m 0755 $(@D)/libfoo.so* $(STAGING_DIR)/usr/lib
+# endef
+define EEMBC_MULTIBENCH_INSTALL_TARGET_CMDS
+	-mkdir -m 0755 $(TARGET_DIR)/root/multibench/
+	cp -r $(@D)/builds/linux/bin/ $(TARGET_DIR)/root/multibench/
+	cp -r $(@D)/builds/linux/data/ $(TARGET_DIR)/root/multibench/
+	cp $(@D)/multibench.sh $(TARGET_DIR)/root/multibench/bin/
+endef
+$(eval $(generic-package))
